f919de7b84c3445c3162a1acd425f69a42df00c3
Ziad Ahmed <ziadelnady555@gmail.com>
2024-01-01 00:18:48 +0200

    created useLocalStorage hook

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,7 @@ export default function Home() {
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 m-8 gap-6">
           {products.map((product: any) => {
             return <ProductCard data={product} />
-          }
-          )}
+          })}
         </div>
       </Container>
     </div>
